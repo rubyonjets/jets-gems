d96c252701cfee7284b47199d15ac7c99fa6b820
Tung Nguyen <tongueroo@gmail.com>
2018-11-01 16:48:33 +0000

    init version

--- lib/jets/gems.rb
+++ lib/jets/gems.rb
@@ -2,6 +2,15 @@ require "jets/gems/version"
 
 module Jets
   module Gems
-    # Your code goes here...
+    autoload :Check, 'jets/gems/check'
+    autoload :Exist, 'jets/gems/exist'
+    autoload :Extract, 'jets/gems/extract'
+    autoload :Report, 'jets/gems/report'
+
+    def ruby_version_folder
+      major, minor, patch = RUBY_VERSION.split('.')
+      [major, minor, '0'].join('.') # 2.5.1 => 2.5.0
+    end
+    extend self
   end
 end
--- spec/jets/gems_spec.rb
+++ spec/jets/gems_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Jets::Gems do
   it "has a version number" do
     expect(Jets::Gems::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
