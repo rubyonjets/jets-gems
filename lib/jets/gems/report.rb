9af141538cf6d28a9c40f923ceb37f91ba440235
Tung Nguyen <tongueroo@gmail.com>
2018-11-01 20:38:05 +0000

    ensure report missing gems finishes

--- lib/jets/gems/report.rb
+++ lib/jets/gems/report.rb
@@ -14,16 +14,20 @@ module Jets::Gems
 
     def report
       version_pattern = /(.*)-(\d+\.\d+\.\d+.*)/
+      threads = []
       @gems.each do |gem_name|
         if md = gem_name.match(version_pattern)
           name, version = md[1], md[2]
-          Thread.new do
+          threads << Thread.new do
             call_api("report/missing?name=#{name}&version=#{version}")
           end
         else
           puts "WARN: Unable to extract the version from the gem name."
         end
       end
+      # Wait for request to finish because the command might finish before
+      # the Threads even send the request. So we join them just case
+      threads.each(&:join)
     end
 
     def api_url(path)
@@ -31,12 +35,12 @@ module Jets::Gems
     end
 
     def call_api(path)
+      # raise "HI"
       uri = URI(api_url(path))
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
+      http.use_ssl = uri.scheme == "https"
       request = Net::HTTP::Get.new(uri)
-      http.request(request) # response
-      resp = http.request(request)
+      response = http.request(request)
       resp = {
         status: response.code.to_i,
         headers: response.each_header.to_h,
