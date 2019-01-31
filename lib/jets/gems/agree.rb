fb5a53bd9a9f707c34d6b60f917ba9e6277b77ec
Tung Nguyen <tongueroo@gmail.com>
2019-01-31 14:58:48 +0000

    no interrupt mode: use JETS_AGREE to bypass prompt

--- lib/jets/gems/agree.rb
+++ lib/jets/gems/agree.rb
@@ -6,6 +6,7 @@ module Jets::Gems
 
     # Only prompts if hasnt prompted before and saved a ~/.jets/agree file
     def prompt
+      return if bypass_prompt
       return if File.exist?(@agree_file)
 
       puts <<~EOL
@@ -17,8 +18,22 @@ module Jets::Gems
       answer = $stdin.gets.strip
       value = answer =~ /y/i ? 'yes' : 'no'
 
-      FileUtils.mkdir_p(File.dirname(@agree_file))
-      IO.write(@agree_file, value)
+      write_file(value)
+    end
+
+    # Allow user to bypass prompt with JETS_AGREE=1 JETS_AGREE=yes etc
+    # Useful for CI/CD pipelines.
+    def bypass_prompt
+      agree = ENV['JETS_AGREE']
+      return false unless agree
+
+      if %w[1 yes true].include?(agree.downcase)
+        write_file('yes')
+      else
+        write_file('no')
+      end
+
+      true
     end
 
     def yes?
